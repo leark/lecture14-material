7908a6d691b1aca99efe822b6d65ca9882959c7b
Joel Ross <joelross@uw.edu>
2017-05-11 12:46:18 -0700

    Reset for lecture start

--- app/src/main/java/edu/uw/materialdemo/MainActivity.java
+++ app/src/main/java/edu/uw/materialdemo/MainActivity.java
@@ -1,24 +1,15 @@
 package edu.uw.materialdemo;
 
-import android.app.ActivityOptions;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "Main";
-
-    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,38 +19,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, fab, "fab"); //link single item
-                startActivity(new Intent(MainActivity.this, ScrollingActivity.class), options.toBundle());
-
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -71,28 +33,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
             case R.id.menu_slideshow:
+                Log.v(TAG, "Clicked slideshow menu");
 
-                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this, fab, "fab");
-
-                // start the new activity
-                startActivity(new Intent(MainActivity.this, ScrollingActivity.class), options.toBundle());
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        switch(item.getItemId()){
-            case R.id.menu_slideshow:
-                startActivity(new Intent(MainActivity.this, ScrollingActivity.class));
-                break;
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START); //close the drawer
-        return true; //always assume handled
     }
 }
