f426827e816fa2d320db215df081aacc1938bd4e
Seung Lee <shl7@uw.edu>
2017-05-11 18:10:20 -0700

    Added lecture code

--- app/src/main/java/edu/uw/materialdemo/MainActivity.java
+++ app/src/main/java/edu/uw/materialdemo/MainActivity.java
@@ -1,11 +1,20 @@
 package edu.uw.materialdemo;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +27,32 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "You clicked me!");
+//                Snackbar.make(v, "you clicked me!", Snackbar.LENGTH_SHORT).show();
+//
+//                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.nav_drawer);
+//                drawer.openDrawer(GravityCompat.START);
+
+                Intent intent = new Intent(MainActivity.this, ScrollingActivity.class);
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, fab, "fab");
+                startActivity(intent, options.toBundle());
+            }
+        });
+
+        NavigationView navView = (NavigationView)findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+                return false;
+            }
+        });
 
     }
 
